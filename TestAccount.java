10cd4bfbf4fff520758aad0bd63731a54cdc88bf
murillomasson <97128702+murillomasson@users.noreply.github.com>
2022-09-13 01:19:04 -0300

    Update TestAccount.java

--- TestAccount.java
+++ TestAccount.java
@@ -4,21 +4,9 @@ public class TestAccount {
 	
 	public static void main(String[] args) {
 		
-		accountOp user001 = new accountOp();
-		accountOp user002 = new accountOp();
-
-		user001.nameAccount = "Jo„o da Silva";
-		user001.numAccount = 1234567;
-		user001.balanceAccount = 452.559;
-		user001.limitOverdraftAccount = 150.00;
-		user001.overdraftUse = false;
-
-		user002.nameAccount = "Maria da Silva";
-		user002.numAccount = 2345678;
-		user002.balanceAccount = -242.062;
-		user002.limitOverdraftAccount = 300.00;
-		user002.overdraftUse = true;
-
+		AccountOp user001 = new AccountOp(12345, "Jo√£o da Silva", 452.559, 150.00, false);
+		AccountOp user002 = new AccountOp(54321, "Maria da Silva", 242.062, 300.00, true);
+		
 		user001.welcomeUser();
 		user001.checkBalance();
 		user001.makeDeposit(50);
@@ -42,8 +30,7 @@ public class TestAccount {
 		user002.makeTransfer(2192.45, 2345678);
 		user002.checkBalance();
 		user002.checkOverdraftUse();
-		
-		}
 
+		}
 	
 	}
